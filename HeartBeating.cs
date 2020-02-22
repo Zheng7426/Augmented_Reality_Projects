2403223abb59767587eac6a16eaf6e4956f31078
zheng7426 <lilsam6670@gmail.com>
2020-02-21 21:11:21 -0500

    Scripts for heart anatomy model

---
+++ HeartBeating.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beating : MonoBehaviour
+{
+    public float scaleSpeed = 0.3f;
+    private Vector3 scaleChange;
+
+
+
+    void Start()
+    {
+        scaleChange = new Vector3(-0.01f, -0.01f, -0.01f);
+    }
+
+    void Update()
+    {
+        transform.localScale += scaleSpeed * scaleChange;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            scaleSpeed += 0.01f;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            scaleSpeed -= 0.01f;
+        }
+        if (transform.localScale.y < 0.1f || transform.localScale.y > 1.0f)
+        {
+            scaleChange = -scaleChange;
+        }
+
+
+    }
+}
+
---
+++ HeartRotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartRotate : MonoBehaviour
+{
+    public int rotateSpeed = 2;
+
+
+
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, rotateSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, -2*(rotateSpeed-1), 0);
+        }
+
+    }
+}
