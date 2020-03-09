4adb08dc0d31a8a777e0bd5c6c8bd11d551c9a02
zheng7426 <lilsam6670@gmail.com>
2020-03-09 14:30:50 -0400

    recycleBin project scripts

---
+++ RecycleBin/ButtonPress.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour
+{
+
+    //Add a declaration of gameobject array
+    public Transform position;
+    public GameObject[] newStuff; 
+    public bool Variable = true;
+    //public GameObject spawner;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+      
+            if (other.transform.tag == "RightHand")
+            {
+            Debug.Log("enter right");
+            //Add here the Y demotion by 0.03f
+            transform.position = transform.position - new Vector3(0, 0.03f, 0);
+
+            }
+
+            if (other.transform.tag == "LeftHand")
+            {
+
+            //Add here the Y demotion by 0.03f
+            transform.position = transform.position - new Vector3(0, 0.03f, 0);
+
+        }
+
+            
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag == "RightHand")
+                {
+            transform.position = transform.position + new Vector3(0, 0.03f, 0);
+            //Add here the Y elevation by 0.03f
+
+            if (Variable)
+                     {
+                    int newItemIndex = Random.Range(0, 4);
+                    GameObject newItem = newStuff[newItemIndex];
+                    Instantiate(newItem, new Vector3(0.422f, 1.1251f, 1.556f), Quaternion.identity); ;
+                    Variable = false;
+                     }
+                }
+
+                if (other.transform.tag == "LeftHand")
+                {
+                transform.position = transform.position + new Vector3(0, 0.03f, 0);
+                //Add here the Y elevation by 0.03f
+
+                if (Variable) {
+                    int newItemIndex = Random.Range(0, 4);
+                    GameObject newItem = newStuff[newItemIndex];
+                    Instantiate(newItem, new Vector3(0.422f, 1.1251f, 1.556f), Quaternion.identity);
+
+                    //Add here a Random generation of objects
+                    Variable = false;
+
+                    }
+                }
+
+    }
+
+
+}
+
+   
+
---
+++ RecycleBin/TestObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    GameObject Button;
+    public string OwnTag ;
+    void Start()
+    {
+        
+        Button  = GameObject.Find("button");
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        string objectTag = other.transform.tag;
+
+        if (objectTag == OwnTag) {
+
+            //Implement the destruction
+            Destroy(this);
+			//Explain what this line do
+            Button.GetComponentInChildren<ButtonPress>().Variable = true;
+            // Using a VARIABLE parameter in the ButtonPress scirpt allows to check zhether or not the last istantiated object has been 
+            //This updates the Variable in the ButtonPress script back to 'True'. So that pressing the button could now instantiate a new object again. 
+        }
+    }
+}
